49d62d430adf32c18fd0daa353b20a73ffe31293
oo33 <hasnaouiyacine59@gmail.com>
2026-04-18 16:19:13 +0100

    fix_bulk1

--- cum.py
+++ cum.py
@@ -1,7 +1,7 @@
 import os, json, time, random, requests, argparse, platform, uuid, socket
 os.environ['CAMOUFOX_NO_UPDATE'] = '1'
 
-VERSION = "v4.0.3"
+VERSION = "v5.0.0"
 BANNER = f"""
   ███╗   ██╗ ██████╗ ██╗   ██╗ █████╗     ██████╗ ██╗███╗   ██╗
   ████╗  ██║██╔═══██╗██║   ██║██╔══██╗    ██╔══██╗██║████╗  ██║
